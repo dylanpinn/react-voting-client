6f134647103042b3104edab8d7e605a3cfd753f0
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-09-10 22:51:17 +1000

    Add flow config

---
+++ src/components/Voting.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Voting from './Voting';
+import {
+  renderIntoDocument,
+  scryRenderedDOMComponentsWithTag,
+  Simulate,
+} from 'react-addons-test-utils';
+
+it('renders without crashing', () => {
+
+});
