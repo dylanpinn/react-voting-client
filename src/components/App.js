9dafbfbb9ce434df2ed8f7321136350f0d9cb6d7
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-09-10 23:37:46 +1000

    Add results page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 /** @flow */
 
 import React from 'react';
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 
 const pair = List.of('Trainspotting', '28 Days Later');
+const tally = Map({'Trainspotting': 5, '28 Days Later': 4});
 
 export default React.createClass({
   render: function() {
-    return React.cloneElement(this.props.children, { pair: pair });
+    return React.cloneElement(this.props.children, {
+      pair: pair,
+      tally: tally
+    });
   }
 });
--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
   <Route path="/" component={Voting} />
 </Route>;
 
