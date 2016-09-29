7b35722566bcc03a9217e0733a708d51b5506fd5
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-09-29 23:30:59 +1000

    Add action_creators tests

---
+++ src/actions/action_creators.test.js
@@ -0,0 +1,30 @@
+import * as actions from './action_creators';
+
+describe('actions', () => {
+  it('should create an action to set state', () => {
+    const state = {};
+    const expectedAction = {
+      type: 'SET_STATE',
+      state
+    };
+    expect(actions.setState(state)).toEqual(expectedAction);
+  });
+
+  it('should create an action to vote', () => {
+    const entry = {};
+    const expectedAction = {
+      type: 'VOTE',
+      meta: { remote: true },
+      entry
+    };
+    expect(actions.vote(entry)).toEqual(expectedAction);
+  });
+
+  it('should create an action for next', () => {
+    const expectedAction = {
+      type: 'NEXT',
+      meta: { remote: true }
+    };
+    expect(actions.next()).toEqual(expectedAction);
+  });
+});
