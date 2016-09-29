f530c36f20e0e0bba73a7a902f954ee6f27a7232
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-09-29 23:54:47 +1000

    Clean up Results component

--- src/components/Results.js
+++ src/components/Results.js
@@ -8,19 +8,19 @@ export const VOTE_WIDTH_PERCENT = 8;
 
 export const Results = React.createClass({
   mixins: [PureRenderMixin],
-  getPair () {
+  getPair() {
     return this.props.pair || [];
   },
-  getVotes (entry) {
+  getVotes(entry) {
     if (this.props.tally && this.props.tally.has(entry)) {
       return this.props.tally.get(entry);
     }
     return 0;
   },
-  getVotesBlockWidth (entry) {
-    return `${this.getVotes(entry) * VOTE_WIDTH_PERCENT  }%`;
+  getVotesBlockWidth(entry) {
+    return `${this.getVotes(entry) * VOTE_WIDTH_PERCENT}%`;
   },
-  render () {
+  render() {
     return this.props.winner ?
       <Winner ref="winner" winner={this.props.winner} /> :
       <div className="results">
@@ -31,8 +31,7 @@ export const Results = React.createClass({
               <div className="voteVisualization">
                 <div className="votesBlock"
                   style={{ width: this.getVotesBlockWidth(entry) }}
-                >
-                </div>
+                 />
               </div>
               <div className="voteCount">
                 {this.getVotes(entry)}
